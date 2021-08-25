4e75ae0d96c5b00a3e9b42fdae3d25ca273a4df9
Max Eliseev <mxseev@gmail.com>
2021-08-25 18:34:05 +0500

    feat: добавлен вывод адреса локального сервера в консоль при npm run dev

--- src/dev-server.ts
+++ src/dev-server.ts
@@ -4,8 +4,13 @@ import http from "http"
 
 import apolloRoute from "../api/index"
 
+const HOST = "localhost"
+const PORT = 3000
+
 const server = new http.Server((request, response) => {
   apolloRoute(request as any, response as any)
 })
 
-server.listen(3000)
+server.listen(PORT, HOST, () => {
+  console.log(`Apollo Server started at: http://${HOST}:${PORT}`)
+})
