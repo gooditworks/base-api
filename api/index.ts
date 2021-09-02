b160f68aa33c3849cf6f19da9c80252d9d89a966
Max Eliseev <mxseev@gmail.com>
2021-09-02 07:03:35 +0500

    refactor: перенос CORS заголовков из vercel.json в хендлер

--- api/index.ts
+++ api/index.ts
@@ -8,6 +8,11 @@ const handler: VercelApiHandler = async (request, response) => {
   await startPromise
   const apolloHanlder = server.createHandler({path: "/"})
 
+  response.setHeader("Access-Control-Allow-Origin", "https://studio.apollographql.com")
+  response.setHeader("Access-Control-Allow-Methods", "OPTIONS,POST")
+  response.setHeader("Access-Control-Allow-Headers", "Content-Type")
+  response.setHeader("Access-Control-Allow-Credentials", "true")
+
   if (request.method === "OPTIONS") {
     return response.status(200).end()
   }
