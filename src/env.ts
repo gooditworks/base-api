dafd5e9d395bb7079a31aa791cfc280bf1a9bb1b
Max Eliseev <mxseev@gmail.com>
2021-09-03 16:19:25 +0500

    feat: удален gh action `schema-publish`, репортом схемы теперь занимается сам Apollo Server

--- src/env.ts
+++ src/env.ts
@@ -7,8 +7,13 @@ const env = {
   sentryDsn: process.env.SENTRY_DSN,
   logdnaKey: process.env.LOGDNA_KEY,
   logdnaApp: "base",
+  apolloKey: process.env.APOLLO_KEY,
+  apolloGraphId: process.env.APOLLO_GRAPH_ID,
   apolloExplorer: process.env.APOLLO_EXPLORER,
-  apolloIntrospection: process.env.APOLLO_INTROSPECTION
+  apolloIntrospection: process.env.APOLLO_INTROSPECTION,
+  vercel: Boolean(process.env.VERCEL),
+  vercelBranch: process.env.VERCEL_GIT_COMMIT_REF,
+  vercelCommit: process.env.VERCEL_GIT_COMMIT_SHA
 }
 
 export default env
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import {ApolloServer} from "apollo-server-micro"
 import {
   ApolloServerPluginLandingPageGraphQLPlayground,
-  ApolloServerPluginLandingPageProductionDefault
+  ApolloServerPluginLandingPageProductionDefault,
+  ApolloServerPluginSchemaReporting
 } from "apollo-server-core"
 
 import env from "./env"
@@ -16,6 +17,19 @@ if (env.apolloExplorer === "true") {
   plugins.push(ApolloServerPluginLandingPageGraphQLPlayground())
 } else {
   plugins.push(ApolloServerPluginLandingPageProductionDefault())
+}
+
+if (env.vercel && env.apolloKey && env.apolloGraphId) {
+  process.env.APOLLO_GRAPH_VARIANT = env.vercelBranch
+
+  process.env.APOLLO_SERVER_PLATFORM = "vercel"
+  process.env.APOLLO_SERVER_USER_VERSION = env.vercelCommit
+
+  plugins.push(
+    ApolloServerPluginSchemaReporting({
+      initialDelayMaxMs: 30_000
+    })
+  )
 }
 
 const introspection = env.apolloIntrospection === "true"
