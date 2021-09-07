49eba0e0c5656c1378bf7a6a70c6ec330509bb6f
Max Eliseev <mxseev@gmail.com>
2021-09-07 15:22:20 +0500

    feat: добавлена система автоматического нагрузочного тестирования k6

---
+++ k6/empty.js
@@ -0,0 +1,34 @@
+import http from "k6/http"
+import {check, sleep} from "k6"
+
+const URL = process.env.VERCEL_URL
+
+const options = {
+  stages: [
+    {duration: "20s", target: 25},
+    {duration: "20s", target: 50},
+    {duration: "20s", target: 75},
+    {duration: "20s", target: 100},
+    {duration: "20s", target: 0}
+  ]
+}
+
+const empty = () => {
+  const body = JSON.stringify({
+    query: "query K6_Empty { health { empty } }"
+  })
+
+  const headers = {
+    "Content-Type": "application/json"
+  }
+
+  const response = http.post(URL, body, {headers}).json()
+  check(response, {
+    "response valid": r => r.data.health.empty === "void"
+  })
+
+  sleep(1)
+}
+
+export {options}
+export default empty
