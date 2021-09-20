040ad0d98c29f8de03fddc4eb6ea1ead71c5aba6
Max Eliseev <mxseev@gmail.com>
2021-09-20 12:59:19 +0500

    chore: из CORS удален фильтр по методу запроса

--- api/index.ts
+++ api/index.ts
@@ -12,9 +12,8 @@ const handler: VercelApiHandler = async (request, response) => {
   const requestOrigin = request.headers.origin || ""
   if (env.corsOrigins.includes(requestOrigin)) {
     response.setHeader("Access-Control-Allow-Origin", requestOrigin)
-    response.setHeader("Access-Control-Allow-Methods", "OPTIONS,POST")
-    response.setHeader("Access-Control-Allow-Headers", "Content-Type,gcdn-debug")
     response.setHeader("Access-Control-Allow-Credentials", "true")
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type,gcdn-debug")
   }
 
   if (request.method === "OPTIONS") {
